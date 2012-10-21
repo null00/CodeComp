253847e5376e6cc62a2f8a96e7f1530e47a6e487
null <null@student.agh.edu.pl>
2012-10-21 23:57:42 +0200

    Enabled filtering

--- codecomp/src/main/java/pl/edu/agh/codecomp/file/actions/AddFileAction.java
+++ codecomp/src/main/java/pl/edu/agh/codecomp/file/actions/AddFileAction.java
@@ -9,20 +9,24 @@ import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
 import pl.edu.agh.codecomp.file.CCFileReader;
+import pl.edu.agh.codecomp.filter.Filter;
 import pl.edu.agh.codecomp.gui.CodeCompGUI;
 
 public class AddFileAction implements ActionListener {
 
+	private final String PROJECT_PATH = System.getProperty("user.dir") + "/src/main/java/";
+
 	public void actionPerformed(ActionEvent ae) {
 		Reader reader = addFile();
+		String text = Filter.parse(readFile(reader));
 		if (reader != null) {
 			switch (ae.getActionCommand()) {
 			case "leftText": {
-				CodeCompGUI.setLeftFile(reader);
+				CodeCompGUI.setLeftFile(text);
 				break;
 			}
 			case "rightText": {
-				CodeCompGUI.setRightFile(reader);
+				CodeCompGUI.setRightFile(text);
 				break;
 			}
 			default:
@@ -36,6 +40,7 @@ public class AddFileAction implements ActionListener {
 		try {
 			JFileChooser jc = new JFileChooser("Add file");
 			jc.setApproveButtonText("Add");
+			jc.setCurrentDirectory(new File(PROJECT_PATH));
 			jc.setFileFilter(new FileNameExtensionFilter("Source Code Files", "asm", "java", "c", "ccp", "py", "jy", "html", "css"));
 			int ret = jc.showOpenDialog(CodeCompGUI.getMainWin());
 			if (ret == JFileChooser.APPROVE_OPTION) {
@@ -47,5 +52,18 @@ public class AddFileAction implements ActionListener {
 			ex.printStackTrace();
 		}
 		return reader;
+	}
+
+	private String readFile(Reader reader) {
+		int c;
+		String output = "";
+		try {
+			while ((c = reader.read()) != -1) {
+				output += String.valueOf(c);
+			}
+		} catch (Exception ex) {
+			//TODO: LOGGER
+		}
+		return output;
 	}
 }
