a6c6a6fc1b2cff6542d61e598d22f923b8de665d
Jaroslaw Szczesniak <szczesniak.jarek@gmail.com>
2013-01-25 10:33:05 +0000

    Removed nused import

--- codecomp/src/main/java/pl/edu/agh/codecomp/algorithm/IAlgorithm.java
+++ codecomp/src/main/java/pl/edu/agh/codecomp/algorithm/IAlgorithm.java
@@ -2,8 +2,6 @@ package pl.edu.agh.codecomp.algorithm;
 
 import java.util.List;
 
-import org.omg.CORBA.Object;
-
 public interface IAlgorithm {
 
 	public abstract List<Integer> match(String text, String pattern);
