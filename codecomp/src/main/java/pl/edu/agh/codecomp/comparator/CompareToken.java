d11788bedd14fbe0fd72d4911743f53e57f5398a
Jaroslaw Szczesniak <szczesniak.jarek@gmail.com>
2013-01-28 16:53:01 +0000

    New options for lowerCase

--- codecomp/src/main/java/pl/edu/agh/codecomp/comparator/CompareToken.java
+++ codecomp/src/main/java/pl/edu/agh/codecomp/comparator/CompareToken.java
@@ -6,6 +6,8 @@ import pl.edu.agh.codecomp.algorithm.KarpRabin;
 
 public class CompareToken {
 
+	private static Boolean filter = true;
+	private static Boolean lowerCase = true;
 	private static IAlgorithm[] algoList = { new BoyerMoore(), new KarpRabin() };
 	private static IAlgorithm textAlgorithm;
 	static {
@@ -26,6 +28,22 @@ public class CompareToken {
 
 	public static void setAlgoList(IAlgorithm[] algoList) {
 		CompareToken.algoList = algoList;
+	}
+	
+	public static Boolean getFilter() {
+		return filter;
+	}
+
+	public static void setFilter(Boolean filter) {
+		CompareToken.filter = filter;
+	}
+
+	public static Boolean getToLowerCase() {
+		return lowerCase;
+	}
+
+	public static void setToLowerCase(Boolean lowerCase) {
+		CompareToken.lowerCase = lowerCase;
 	}
 	
 }
